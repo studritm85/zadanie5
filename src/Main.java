02fe11d3b562fc3ebd4f28557a2d275e0c89fc65
Ekaterina <studritm@gmail.com>
2024-05-04 22:58:24 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService bmiService = new BmiService();
 
-        double weight = 98; // вес в кг
-        double height = 1.87; // рост в метрах
+        double weight = 90; // вес в кг
+        double height = 1.74; // рост в метрах
 
         int bmiIndex = bmiService.calculate(weight, height);
 
